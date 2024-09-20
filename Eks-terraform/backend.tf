6915a4bbbcb78ec9b496de68101546a8f6653aee
pellurimadhu <39980747+pellurimadhu@users.noreply.github.com>
2024-09-20 11:58:59 +0530

    correct bucket

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "http://52.201.215.118:9000/" # Replace with your actual S3 bucket name
+    bucket = "terraform-backendweq" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-1"
   }
